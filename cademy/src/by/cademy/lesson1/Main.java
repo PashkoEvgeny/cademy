3d5bbfc68b89a035e69bb7866047443dfea14ac0
PashkoEvgeny <49685691+PashkoEvgeny@users.noreply.github.com>
2021-01-19 10:42:45 +0300

    Update Main.java

--- cademy/src/by/cademy/lesson1/Main.java
+++ cademy/src/by/cademy/lesson1/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("23redfgd");
-System.out.println("izmenenie na gite");
+System.out.println("izmenenie na gite super GOOD");
 	}
 
 }
