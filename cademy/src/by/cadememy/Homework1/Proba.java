2ea702e739631ee06b4db92f708874f8d8c379f1
Evgeny <Evgeny@Evgeny>
2021-01-19 10:40:01 +0300

    12312

---
+++ cademy/src/by/cadememy/Homework1/Proba.java
@@ -0,0 +1,13 @@
+package by.cadememy.Homework1;
+
+public class Proba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("ads122");
+System.out.println("ads12122");
+int a=5;
+System.out.println(a);
+	}
+
+}
