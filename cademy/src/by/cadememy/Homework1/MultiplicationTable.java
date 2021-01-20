c1447514ae436f2032c7d9057f14cd89532034ba
Evgeny <Evgeny@Evgeny>
2021-01-20 15:24:52 +0300

    nf,kbwf

---
+++ cademy/src/by/cadememy/Homework1/MultiplicationTable.java
@@ -0,0 +1,25 @@
+package by.cadememy.Homework1;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number;
+		int i=1;
+		
+		try (Scanner input = new Scanner(System.in)) {
+		System.out.print("Multiplication Table: ");
+		number = input.nextInt();
+		
+		while (i<=10) {
+			System.out.println(i+" x "+number+" = "+i*number);
+			i=i+1;
+		}
+}
+		
+	}
+;
+}
