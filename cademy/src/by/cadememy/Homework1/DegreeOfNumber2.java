31f83d476096b3bd938f60a446563212a4a8e562
Evgeny <Evgeny@Evgeny>
2021-01-20 15:53:40 +0300

    15:53

---
+++ cademy/src/by/cadememy/Homework1/DegreeOfNumber2.java
@@ -0,0 +1,18 @@
+package by.cadememy.Homework1;
+
+import java.util.Scanner;
+
+public class DegreeOfNumber2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=0;
+		
+		while (Math.pow(2,i)<=100) {
+			System.out.println("2 v stepeni "+i+" = "+Math.pow(2,i));
+			i=i+1;
+		}	
+}
+	}
+
