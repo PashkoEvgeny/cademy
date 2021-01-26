42c83bfb2bb664e47637fc94b92a49fa97daed38
Evgeny <Evgeny@Evgeny>
2021-01-26 13:40:20 +0300

    123

--- cademy/src/by/cadememy/Homework1/CaseSwitch.java
+++ cademy/src/by/cadememy/Homework1/CaseSwitch.java
@@ -1,39 +1,15 @@
 package by.cadememy.Homework1;
 
-import java.util.Arrays;
-import java.util.Random;
-
 public class CaseSwitch {
 	
-	public static void sort(int arr[]) {
-		for (int q=0; q<arr.length; q++ ) {
-		for (int i=0; i<arr.length-1; i++) {
-			
-			if (arr[i]>arr[i+1]) {
-				int f;
-				f=arr[i+1];
-				arr[i+1]=arr[i];
-				arr[i]=f;
-			}
-		}
-		
-	}
-	}
-
 	public static void main(String[] args) {
-
-		int[] array= new int[10];
-		Random rand=new Random();
-		
-		for (int i=0; i<array.length; i++) {
-			array[i]=rand.nextInt(10);
-		}
-		System.out.println(Arrays.toString(array));
-		
-		sort(array);
-		
-		System.out.println(Arrays.toString(array));
-		
+		System.out.println("Hello");
+	
+		/*
+		 * Задача 2.
+Создайте массив из всех нечётных чисел от 1 до 99, 
+выведите его на экран в строку, а затем этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 … 7 5 3 1).
+		 */
 	}
 
 }
