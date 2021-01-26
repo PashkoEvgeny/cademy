97410504eff3ae63f71a6bce840bc822c2b9998b
Evgeny <Evgeny@Evgeny>
2021-01-26 15:49:45 +0300

    123

--- cademy/src/by/cadememy/Homework1/CaseSwitch.java
+++ cademy/src/by/cadememy/Homework1/CaseSwitch.java
@@ -1,15 +1,41 @@
 package by.cadememy.Homework1;
 
+import java.util.Arrays;
+
 public class CaseSwitch {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hello");
-	
-		/*
-		 * Задача 2.
-Создайте массив из всех нечётных чисел от 1 до 99, 
-выведите его на экран в строку, а затем этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 … 7 5 3 1).
-		 */
-	}
 
+		int n = 0;
+
+		for (int i = 1; i <= 99; i++) {
+			if (i % 2 == 1) {
+				n++;
+			}
+
+		}
+		System.out.println(n);
+
+		int[] array = new int[n];
+
+		System.out.println(Arrays.toString(array));
+
+	
+		for (int r = 1, q=0;r <= 99; r++) {
+			if (r % 2 == 1) {
+				array[q] = r;
+				q++;
+
+			}
+
+		}
+
+		System.out.println(Arrays.toString(array));
+
+		for (int a = array.length - 1; a >= 0; a--) {
+			System.out.print(array[a] + "  ");
+		}
+
+	}
 }
