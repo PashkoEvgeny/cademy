d3ff8856b6549c2b44afb3b49f6a3a2cc2c0bf7a
Evgeny <Evgeny@Evgeny>
2021-01-27 13:13:14 +0300

    12

---
+++ cademy/src/by/cademy/classwork/lesson1/Proizved.java
@@ -0,0 +1,38 @@
+package by.cademy.classwork.lesson1;
+
+public class Proizved {
+
+	public static void main(String[] args) {
+
+
+				long n=   1;
+				
+				for (int i =0;i<30; i++) {
+					if (i%13==0) {
+						n=n*i;
+					}
+					
+				}
+				
+				System.out.println("Result: " + n);
+				
+				*/
+				
+				/*
+				long result = 1;
+
+				for (int i = 11; i < 100; i += 2) {
+					if (i % 13 == 0) {
+						result = result * i;
+					}
+				}
+
+				System.out.println("Result: " + result);
+				*/
+			}
+
+}
+
+}
+
+}
