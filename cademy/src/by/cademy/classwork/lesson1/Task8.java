f136820a98b4243d8b38cefed137bcfb089085d3
Evgeny <Evgeny@Evgeny>
2021-01-27 13:56:36 +0300

    new

---
+++ cademy/src/by/cademy/classwork/lesson1/Task8.java
@@ -0,0 +1,15 @@
+package by.cademy.classwork.lesson1;
+
+public class Task8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		/*
+		 * Создайте массив из 12 случайных целых чисел из отрезка [-15;15]. 
+			Определите какой элемент является в этом массиве максимальным и 
+			сообщите индекс его последнего вхождения в массив.
+		 */
+	}
+
+}
