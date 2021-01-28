ce5b54bb7e31016e0ec6bc193013a46d3ecbe394
Evgeny <Evgeny@Evgeny>
2021-01-28 12:58:25 +0300

    Task1

---
+++ cademy/src/by/cadememy/Homework2/Task1.java
@@ -0,0 +1,19 @@
+package by.cadememy.Homework2;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * ¬вести две строки с консоли. ќпределить, €вл€етс€ ли одна строка перестановкой символов другой строки. 
+		 * ”читываем регистр. Ќе использовать сортировку :) 
+			Ќапример: 
+			1.	УhelloФ и УhlleoФ -> true
+			2.	УhelloФ и УartФ -> false
+		 */
+		
+		
+		
+	}
+
+}
