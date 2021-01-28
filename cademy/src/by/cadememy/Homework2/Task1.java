127eec434fd218b06742a320bec4c0cdc1a290c6
Evgeny <Evgeny@Evgeny>
2021-01-28 17:26:48 +0300

    yes

--- cademy/src/by/cadememy/Homework2/Task1.java
+++ cademy/src/by/cadememy/Homework2/Task1.java
@@ -1,19 +1,45 @@
 package by.cadememy.Homework2;
 
+import java.util.Scanner;
+
 public class Task1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*
-		 * ¬вести две строки с консоли. ќпределить, €вл€етс€ ли одна строка перестановкой символов другой строки. 
-		 * ”читываем регистр. Ќе использовать сортировку :) 
-			Ќапример: 
-			1.	УhelloФ и УhlleoФ -> true
-			2.	УhelloФ и УartФ -> false
-		 */
 		
-		
-		
-	}
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 lines:");
+		String str1 = sc.nextLine();
+		String str2 = sc.nextLine();
+		sc.close();
+
+		StringBuffer str11 = new StringBuffer(str1);
+		StringBuffer str22 = new StringBuffer(str2);
+
+		if (str1.length() == str2.length()) {
+
+			for (int i = 0; i < str1.length(); i++) {
+				for (int q = 0; q < str11.length(); q++) {
+					if (str11.charAt(0) == str22.charAt(q)) {
+						str11 = str11.deleteCharAt(0);
+						str22 = str22.deleteCharAt(q);
+						System.out.println(str11);
+						System.out.println(str22);
+					}
+				}
+			}
+
+		}
+		
+		if (str11.length()==0) {
+			System.out.println("true");
+			
+		}
+		else {
+			System.out.println("false");
+		}
+		
+		
+
+	}
 }
---
+++ cademy/src/by/cadememy/Homework2/Task11.java
@@ -0,0 +1,17 @@
+package by.cadememy.Homework2;
+
+public class Task11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str=("stroka");
+		System.out.println(str);
+		
+		StringBuffer str1 = new StringBuffer(str);
+		
+		str1 = str1.deleteCharAt(5);
+		
+		
+		System.out.println(str1);
+	}
+}
