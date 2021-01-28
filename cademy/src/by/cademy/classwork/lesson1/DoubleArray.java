c8f4bfdc03669e9b3da022113d268831144b730f
Evgeny <Evgeny@Evgeny>
2021-01-28 11:52:49 +0300

    1152

---
+++ cademy/src/by/cademy/classwork/lesson1/DoubleArray.java
@@ -0,0 +1,34 @@
+package by.cademy.classwork.lesson1;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class DoubleArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//	Задача 14.
+	//	Создать двумерный массив из 8 строк по 5 столбцов в каждой из случайных целых чисел из отрезка [10;99]. 
+	//	Вывести массив на экран.
+
+		Random rand=new Random();
+		
+		
+		int[] [] array=new int[8][5];
+		
+		for (int i=0; i<8; i++) {
+			for (int q=0; q<5; q++) {
+				array[i][q]=rand.nextInt(90)+10;
+			}
+			 
+		}
+		
+	//	Arrays.sort(array);
+		System.out.println("ot " + array[0][0]+ " do "+ array[7][4] );
+		System.out.println(Arrays.deepToString(array));
+		
+		
+	}
+
+}
