f7d59f50951b34cfb249ec56eada11bb3cf78378
Evgeny <Evgeny@Evgeny>
2021-01-28 10:04:50 +0300

    ок

--- cademy/src/by/cademy/classwork/lesson1/Cat.java
+++ cademy/src/by/cademy/classwork/lesson1/Cat.java
@@ -2,9 +2,30 @@ package by.cademy.classwork.lesson1;
 
 public class Cat {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int age;
+	String nickname;
 
+	public void grow() {
+		age = age + 1;
+	}
+
+	public void sleep() {
+		System.out.println("cat sleep");
+	}
+
+	public void eat() {
+		System.out.println("cat eat");
+	}
+
+	public void walk() {
+		System.out.println("cat walk");
+	}
+	
+	public Cat() {
+		
+	}
+	public Cat(String nickname) {
+		
 	}
 
 }
