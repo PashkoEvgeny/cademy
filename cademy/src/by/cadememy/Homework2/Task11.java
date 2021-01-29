e98b211806cdb4780867ad752c44322b7ab48108
Evgeny <Evgeny@Evgeny>
2021-01-29 12:45:53 +0300

    1245

--- cademy/src/by/cadememy/Homework2/Task11.java
+++ cademy/src/by/cadememy/Homework2/Task11.java
@@ -3,15 +3,13 @@ package by.cadememy.Homework2;
 public class Task11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String str=("stroka");
-		System.out.println(str);
-		
-		StringBuffer str1 = new StringBuffer(str);
-		
-		str1 = str1.deleteCharAt(5);
+
+		String word1 = ("qwerty");
+		System.out.println(word1);
+		word1 = word1.substring(0, word1.length()/2);
+		System.out.println(word1);
 		
 		
-		System.out.println(str1);
+
 	}
 }
