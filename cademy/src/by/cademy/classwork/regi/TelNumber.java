397ad98f54c036c0fd30b0cce43940722a1ab502
Evgeny <Evgeny@Evgeny>
2021-02-03 13:02:35 +0300

    tel

---
+++ cademy/src/by/cademy/classwork/regi/TelNumber.java
@@ -0,0 +1,21 @@
+package by.cademy.classwork.regi;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TelNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String text = "+375295673108";
+
+		Pattern pattern = Pattern.compile("[+]{1}\\d+");
+
+		Matcher matcher = pattern.matcher(text);
+
+		System.out.println(matcher.matches());
+
+	}
+
+}
