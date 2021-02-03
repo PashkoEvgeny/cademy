da55e0d62e258d57b040141439b6b62b4a0e3b38
Evgeny <Evgeny@Evgeny>
2021-02-03 16:52:22 +0300

    task 3

--- cademy/src/by/cadememy/Homework3/Task3.java
+++ cademy/src/by/cadememy/Homework3/Task3.java
@@ -1,5 +1,6 @@
 package by.cadememy.Homework3;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,23 +8,20 @@ public class Task3 {
 
 	public static void main(String[] args) {
 
-		/*
-		 * 3. Написать класс, который проверял бы введенную дату на соответствие
-		 * патерна: (использовать regexp) a) dd/MM/yyyy b) dd-MM-yyyy
-		 * 
-		 */
+		System.out.print("Enter the date: ");
+		Scanner sc = new Scanner(System.in);
+		String text = sc.nextLine();
 
-		String text = "23-12-2021";
-		
 		// проверка на кол-во дней в месяце
+		// проверка на високосный год
 
-		Pattern pattern = Pattern.compile("((([0-2][\\d])|(30)|(31))\\-(([0][\\d])|([1][0-2]))\\-((19|20)\\d\\d))|"
-				+ "((([0-2][\\d])|(30)|(31))\\/(([0][\\d])|([1][0-2]))\\/((19|20)\\d\\d))");
+		Pattern pattern = Pattern.compile("((([0-2]\\d)|(30)|(31))\\-((0\\d)|(1[0-2]))\\-((19|20)\\d\\d))|"
+				+ "((([0-2]\\d)|(30)|(31))\\/((0\\d)|(1[0-2]))\\/((19|20)\\d\\d))");
 
 		Matcher matcher = pattern.matcher(text);
 
 		System.out.println(matcher.matches());
-
+		sc.close();
 	}
 
 }
