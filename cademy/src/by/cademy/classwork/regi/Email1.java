2baea00eecf524a87589988e521d3c0b946d6f26
Evgeny <Evgeny@Evgeny>
2021-02-03 12:52:21 +0300

    email

--- cademy/src/by/cademy/classwork/regi/Email1.java
+++ cademy/src/by/cademy/classwork/regi/Email1.java
@@ -3,16 +3,22 @@ package by.cademy.classwork.regi;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class Email1 {
 
 	public static void main(String[] args) {
 		
-		Pattern pattern = Pattern.compile("");
-		Matcher matcher = pattern.matcher("");
-		boolean matches = matcher.matches();
+		String text = "pas123hko.belkrug@mail.ru";
+		
+		Pattern pattern = Pattern.compile("(\\w+[\\.-_]?\\w+)*@([a-z]{2,6}\\.{1}[a-z]{2,5})");
+		
+		Matcher matcher = pattern.matcher(text);
 
-		System.out.println(matches);
+		System.out.println(matcher.matches());
+		
 	}
 
 }
+
+// ([a-zA-Z0-9]{1,}[\\-]{0,1}[a-zA-Z0-9]{1,}[\\.]{0,1}[a-zA-Z0-9]{1,})+@([a-z]{2,6}\\.{1}[a-z]{2,3})
+
+// f2fk4klvbg+fg
