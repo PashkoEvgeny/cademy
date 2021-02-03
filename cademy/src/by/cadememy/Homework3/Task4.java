151593b63d9c3c86c21a1a859e59de30479b5a40
Evgeny <Evgeny@Evgeny>
2021-02-03 17:21:23 +0300

    task4

---
+++ cademy/src/by/cadememy/Homework3/Task4.java
@@ -0,0 +1,43 @@
+package by.cadememy.Homework3;
+
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Task4 {
+
+	public static void main(String[] args) {
+		
+		/* Конвертер дат
+		Ввести с клавиатуры дату в формате «08/18/2013»
+		Вывести на экран эту дату в виде «AUG 18, 2013».
+		Воспользоваться объектом Date и SimpleDateFormat.
+		*/
+		
+		        
+		        System.out.print("Enter the date: ");
+				Scanner sc = new Scanner(System.in);
+				String tempDate = sc.nextLine();
+
+		        //в объект simpleFormat записываем тот формат, который нужен
+		        
+		        SimpleDateFormat simpleFormat = new SimpleDateFormat("MM dd, yyyy");
+		        
+		        //в объект simpleFormat2 записываем текущий по заданию формат
+		        
+		        SimpleDateFormat simpleFormat2 = new SimpleDateFormat("MM/dd/yyyy");
+		        
+		        //В объект data записываем запарсенную строку
+		        
+		        Date date = simpleFormat2.parse(tempDate);
+		        
+		        //Форматируем строку так, как нужно по заданию
+		        
+		        System.out.println(simpleFormat.format(date).toUpperCase());
+
+		        //Напишите тут ваш код
+		    }
+		}
+
