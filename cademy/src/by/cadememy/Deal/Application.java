27018d7c654f43cf9423b4a0ab175d22b8ff5bb7
Evgeny <Evgeny@Evgeny>
2021-02-11 17:40:54 +0300

    toWork

--- cademy/src/by/cadememy/Deal/Application.java
+++ cademy/src/by/cadememy/Deal/Application.java
@@ -3,22 +3,18 @@ package by.cadememy.Deal;
 public class Application {
 
 	public static void main(String[] args) {
-
-		// Product(String type, Double price, String manufacturer, Integer quantity)
-
 		
+			Person seller = new Person("Evgeny", 200.00, "+375295673108", "ev.pashko89@gmail.com");
+			Person buyer = new Person("Andrey", 300.00, "+375295042335", "Andrey-email@mail.ru");
 
-		Cheese cheese = new Cheese("Cheese", 25.7, "Itali", 2, 7);
-		System.out.println("Type product: " + cheese.type + "   price: " + cheese.price + "   manufacture: "
-				+ cheese.manufacturer + "   quantity:" + cheese.quantity + "   age: " + cheese.age);
+			Deal deal = new Deal("21-11-2020", seller, buyer);
 
-		Wine wine = new Wine("Wine", 24.5, "Sacebeli", 20, "Georgia");
-		System.out.println("Type product: " + wine.type + "   price: " + wine.price + "   manufacture: "
-				+ wine.manufacturer + "   quantity:" + wine.quantity + "   country: " + wine.country);
-		
-		Milk milk = new Milk("Milk", 1.7, "Prostokvashino", 12, 1.5);
-		System.out.println("Type product: " + milk.type + "   price: " + milk.price + "   manufacture: "
-				+ milk.manufacturer + "   quantity:" + milk.quantity + "   percentFat: " + milk.percentFat);
+			deal.addProduct(new Cheese("Cheese", 25.7, "Itali", 2, 7));
+			deal.addProduct(new Wine("Wine", 24.5, "Sacebeli", 20, "Georgia"));
+			deal.addProduct(new Milk("Milk", 1.7, "Prostokvashino", 12, 1.5));
+
+			deal.deal();
+		}
 	}
+		
 
-}
