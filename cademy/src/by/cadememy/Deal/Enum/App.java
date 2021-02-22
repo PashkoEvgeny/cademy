644492e92b9641c331a340a0eeb716f77d653488
Evgeny <Evgeny@Evgeny>
2021-02-22 14:45:18 +0300

    8+

--- cademy/src/by/cadememy/Deal/Enum/App.java
+++ cademy/src/by/cadememy/Deal/Enum/App.java
@@ -1,35 +1,15 @@
 package by.cadememy.Deal.Enum;
 
+import java.util.ArrayList;
+
+import by.cadememy.Deal.Product;
 
 public class App {
 
+	private static final Product Sheese = null;
+
 	public static void main(String[] args) {
-		System.out.println("Hello!!! Create a Deal! Go...");
-		
-		DealEnum dealEnum =DealEnum.d;
-		
-	//	for (DealEnum en: DealEnum.values()) {
-	
-		dealEnum.getMenu();
-		
-		
-		switch (dealEnum) {
-		case a:
-			System.out.println();
-			break;
-		case b:
-			System.out.println("Today Tuesday ");
-			break;
-		case c:
-			System.out.println("Today Wednesday ");
-			break;    
-		case d:
-			System.out.println(dealEnum.getMenu());
-			break;  
-		}
-		
+		ArrayList<Product> menu = new ArrayList<Product>();
+menu.add(0, Sheese);
 	}
-
 }
-
-
