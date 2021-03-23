8b95153290b1f0aa5e29b1a9036bb0b552314a7e
Evgeny <Evgeny@Evgeny>
2021-03-23 10:26:07 +0300

    hw

--- cademy/src/by/cadememy/Homework5/Tester.java
+++ cademy/src/by/cadememy/Homework5/Tester.java
@@ -1,16 +1,14 @@
 package by.cadememy.Homework5;
 
-
 class Tester {
 
- 
-    public static void main(String... args) {  
-    	{ 
-    	    int i = 0; 
-    	    while (i < 10) { 
-    	        System.out.println(i); 
-    	        i++; 
-    	    } 
-    	} 
-}
+	public static void main(String... args) {
+		{
+			int i = 0;
+			while (i < 10) {
+				System.out.println(i);
+				i++;
+			}
+		}
+	}
 }
---
+++ cademy/src/by/cademy/HW7/Task1.java
@@ -0,0 +1,10 @@
+package by.cademy.HW7;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
